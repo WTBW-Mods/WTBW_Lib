9d526663b396af39c6bc8abeef77026870657525
Naxanria <gerbrandnijk@gmail.com>
2020-01-03 20:30:01 +0100

    Area fix?

--- src/main/java/com/wtbw/mods/lib/util/Utilities.java
+++ src/main/java/com/wtbw/mods/lib/util/Utilities.java
@@ -149,9 +149,9 @@ public class Utilities
     
     int hSize = size / 2;
   
-    int startX = dirX == 0 ? pos.getX() - hSize : pos.getX() + size * dirX - 1;
-    int startY = dirY == 0 ? pos.getY() - hSize : pos.getY() + size * dirY - 1;
-    int startZ = dirZ == 0 ? pos.getZ() - hSize : pos.getZ() + size * dirZ - 1;
+    int startX = dirX == 0 ? pos.getX() - hSize : pos.getX() + size * dirX + ((dirX == -1) ? -1 : 0);
+    int startY = dirY == 0 ? pos.getY() - hSize : pos.getY() + size * dirY + ((dirY == -1) ? -1 : 0);
+    int startZ = dirZ == 0 ? pos.getZ() - hSize : pos.getZ() + size * dirZ + ((dirZ == -1) ? -1 : 0);
     int endX = dirX == 0 ? pos.getX() + hSize : pos.getX();
     int endY = dirY == 0 ? pos.getY() + hSize : pos.getY();
     int endZ = dirZ == 0 ? pos.getZ() + hSize : pos.getZ();
