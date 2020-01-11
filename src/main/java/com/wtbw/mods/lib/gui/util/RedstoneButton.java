3713bf5eb2be8bc08a6774f73056c042c5a14bb6
Naxanria <gerbrandnijk@gmail.com>
2020-01-11 18:06:59 +0100

    Updated Redstone button to draw its own background Changed screens with old redstone gui to use default gui

--- src/main/java/com/wtbw/mods/lib/gui/util/RedstoneButton.java
+++ src/main/java/com/wtbw/mods/lib/gui/util/RedstoneButton.java
@@ -21,23 +21,47 @@ public class RedstoneButton<TE extends TileEntity & IRedstoneControlled> extends
 {
   public static final ResourceLocation ICONS = new ResourceLocation(WTBWLib.MODID, "textures/gui/redstone_buttons.png");
   private final static SpriteMap SPRITE_MAP = new SpriteMap(64, ICONS);
+  private final static Sprite SPRITE_BACKGROUND = SPRITE_MAP.getSprite(16, 0, 24, 26);
   private final static Sprite SPRITE_IGNORE = SPRITE_MAP.getSprite(0, getTextureYOffset(RedstoneMode.IGNORE), 16);
   private final static Sprite SPRITE_ON = SPRITE_MAP.getSprite(0, getTextureYOffset(RedstoneMode.ON), 16);
   private final static Sprite SPRITE_OFF = SPRITE_MAP.getSprite(0, getTextureYOffset(RedstoneMode.OFF), 16);
   private final static Sprite SPRITE_PULSE = SPRITE_MAP.getSprite(0, getTextureYOffset(RedstoneMode.PULSE), 16);
   
-  
   private final IRedstoneControlled control;
   private final TE tile;
+  
+  private boolean drawBackdrop = true;
 
   public RedstoneButton(int xPos, int yPos, TE tile)
   {
-    super(xPos, yPos, 18, 18, "", null);
+    super(xPos, yPos, 24, 26, "", null);
   
     this.control = tile;
     this.tile = tile;
   }
-
+  
+  public boolean isDrawBackdrop()
+  {
+    return drawBackdrop;
+  }
+  
+  public RedstoneButton<TE> setDrawBackdrop(boolean drawBackdrop)
+  {
+    this.drawBackdrop = drawBackdrop;
+    if (drawBackdrop)
+    {
+      width = 24;
+      height = 26;
+    }
+    else
+    {
+      width = 18;
+      height = 18;
+    }
+    
+    return this;
+  }
+  
   @Override
   public boolean isHover(int mouseX, int mouseY)
   {
@@ -61,20 +85,7 @@ public class RedstoneButton<TE extends TileEntity & IRedstoneControlled> extends
   @Override
   public void renderButton(int mouseX, int mouseY, float partial)
   {
-//    Minecraft minecraft = Minecraft.getInstance();
-//    minecraft.getTextureManager().bindTexture(WIDGETS_LOCATION);
-//    GlStateManager.color4f(1.0F, 1.0F, 1.0F, this.alpha);
-//    int yOff = this.getYImage(this.isHovered());
-//    GlStateManager.enableBlend();
-//    GlStateManager.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-//    GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-//    this.blit(this.x, this.y, 0, 46 + yOff * 20, this.width / 2, this.height);
-//    this.blit(this.x + this.width / 2, this.y, 200 - this.width / 2, 46 + yOff * 20, this.width / 2, this.height);
-//
-//    GuiUtil.renderTexture(x, y, width / 2, height, 0, 46 + yOff * 20, 256, 256, WIDGETS_LOCATION);
-//    GuiUtil.renderTexture(x + width / 2, y, width / 2, width / 2, 200 - width / 2, 46 + yOff * 20, 256, 256, WIDGETS_LOCATION);
-    
-    GuiUtil.renderButton(x, y, width, height, isHovered, active);
+//    GuiUtil.renderButton(x, y, width, height, isHovered, active);
     
     Sprite sprite;
     switch (control.getRedstoneMode())
@@ -94,9 +105,18 @@ public class RedstoneButton<TE extends TileEntity & IRedstoneControlled> extends
         break;
     }
     
-    sprite.render(x + 1, y + 1);
+    if (drawBackdrop)
+    {
+      SPRITE_BACKGROUND.render(x, y);
+      sprite.render(x + 5, y + 5);
+    }
+    else
+    {
+      sprite.render(x + 1, y + 1);
+    }
+    
 
-    this.renderBg(Minecraft.getInstance(), mouseX, mouseY);
+//    this.renderBg(Minecraft.getInstance(), mouseX, mouseY);
   }
   
   private RedstoneMode getNextMode()
