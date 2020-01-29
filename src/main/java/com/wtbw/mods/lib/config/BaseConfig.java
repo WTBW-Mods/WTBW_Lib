ea026cb99ec0234f012df0caebcc5317e3fa9b16
Sunekaer <kaspar@sunekaer.com>
2020-01-29 18:23:48 +0100

    Fixed #1

--- src/main/java/com/wtbw/mods/lib/config/BaseConfig.java
+++ src/main/java/com/wtbw/mods/lib/config/BaseConfig.java
@@ -1,6 +1,8 @@
 package com.wtbw.mods.lib.config;
 
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 /*
@@ -31,9 +33,46 @@ public abstract class BaseConfig
   {
     return modid + ".config.client." + name;
   }
-  
+
+  protected static String[] decompose(String resourceName, char splitOn) {
+    String[] astring = new String[]{"minecraft", resourceName};
+    int i = resourceName.indexOf(splitOn);
+    if (i >= 0) {
+      astring[1] = resourceName.substring(i + 1, resourceName.length());
+      if (i >= 1) {
+        astring[0] = resourceName.substring(0, i);
+      }
+    }
+
+    return astring;
+  }
+
+  private static boolean isPathValid(String pathIn) {
+    return pathIn.chars().allMatch((p_217857_0_) -> {
+      return p_217857_0_ == 95 || p_217857_0_ == 45 || p_217857_0_ >= 97 && p_217857_0_ <= 122 || p_217857_0_ >= 48 && p_217857_0_ <= 57 || p_217857_0_ == 47 || p_217857_0_ == 46;
+    });
+  }
+
+  /**
+   * Returns true if given namespace only consists of allowed characters.
+   */
+  private static boolean isValidNamespace(String namespaceIn) {
+    return namespaceIn.chars().allMatch((p_217859_0_) -> {
+      return p_217859_0_ == 95 || p_217859_0_ == 45 || p_217859_0_ >= 97 && p_217859_0_ <= 122 || p_217859_0_ >= 48 && p_217859_0_ <= 57 || p_217859_0_ == 46;
+    });
+  }
+
+  /**
+   * Checks if the specified resource name (namespace and path) contains invalid characters.
+   */
+
+  public static boolean isResouceNameValid(String resourceName) {
+    String[] astring = decompose(resourceName, ':');
+    return isValidNamespace(org.apache.commons.lang3.StringUtils.isEmpty(astring[0]) ? "minecraft" : astring[0]) && isPathValid(astring[1]);
+  }
+
   public static boolean isResourceLocation(String resource)
   {
-    return ResourceLocation.isResouceNameValid(resource);
+    return isResouceNameValid(resource);
   }
 }
