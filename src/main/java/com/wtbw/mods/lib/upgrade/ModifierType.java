386f45a4087ad624915d9b1cb9c78294e9f199fa
Naxanria <gerbrandnijk@gmail.com>
2020-03-24 10:04:07 +0100

    Charger and battery recipes added. Capacity upgrades now use the the batteries in teh recipes.

--- src/main/java/com/wtbw/mods/lib/upgrade/ModifierType.java
+++ src/main/java/com/wtbw/mods/lib/upgrade/ModifierType.java
@@ -1,6 +1,7 @@
 package com.wtbw.mods.lib.upgrade;
 
 import com.wtbw.mods.lib.util.TextComponentBuilder;
+import com.wtbw.mods.lib.util.Utilities;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.util.text.ITextComponent;
@@ -17,7 +18,15 @@ public class ModifierType
 {
   public static final ModifierType SPEED = new ModifierType("speed");
   public static final ModifierType POWER_USAGE = new ModifierType("power_usage", false);
-  public static final ModifierType POWER_CAPACITY = new ModifierType("power_capacity");
+  public static final ModifierType POWER_CAPACITY = new ModifierType("power_capacity")
+  {
+    @Override
+    protected ITextComponent typeInfo(float value)
+    {
+      return TextComponentBuilder.create(Utilities.abbreviate((int) value), true).aqua()
+        .next("FE").build();
+    }
+  }.additive().showInfo();
   public static final ModifierType RANGE = new ModifierType("range")
   {
     @Override
@@ -28,6 +37,8 @@ public class ModifierType
         .build();
     }
   }.additive().showInfo();
+  
+  public static final ModifierType TRANSFER = new ModifierType("transfer");
   
   public final String name;
   private final boolean moreIsPositive;
