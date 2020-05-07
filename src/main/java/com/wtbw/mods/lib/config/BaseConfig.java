835baf8ac3ccd870d2071a6f64fa7e4fb100f8e3
Naxanria <gerbrandnijk@gmail.com>
2020-05-07 10:55:46 +0200

    mappings/forge update

--- src/main/java/com/wtbw/mods/lib/config/BaseConfig.java
+++ src/main/java/com/wtbw/mods/lib/config/BaseConfig.java
@@ -1,10 +1,8 @@
 package com.wtbw.mods.lib.config;
 
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.ForgeConfigSpec;
-import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+
 
 /*
   @author: Naxanria
@@ -69,7 +67,7 @@ public abstract class BaseConfig
 
   public static boolean isResouceNameValid(String resourceName) {
     String[] astring = decompose(resourceName, ':');
-    return isValidNamespace(RandomStringUtils.isEmpty(astring[0]) ? "minecraft" : astring[0]) && isPathValid(astring[1]);
+    return isValidNamespace(StringUtils.isEmpty(astring[0]) ? "minecraft" : astring[0]) && isPathValid(astring[1]);
   }
 
   public static boolean isResourceLocation(String resource)
